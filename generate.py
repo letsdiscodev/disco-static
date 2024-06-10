5adc27d2d90a4019ada99a30e9c94031342ce413
Antoine Leclair <antoineleclair@gmail.com>
2024-06-10 19:26:42 -0400

    Markdown site

--- generate.py
+++ generate.py
@@ -1,26 +1,30 @@
 import os
-import randomname
+import markdown2
 
 print("Generating site")
 
 DIST_DIR = "dist" # /code/dist
-
-HTML = f"""<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-</head>
-<body>
-    <h1>Hello!</h1>
-    <p>This is a random name for this deployment:</p>
-    <p>{randomname.get_name()}</p>
-</body>
-</html>"""
+PAGES_PATH = "pages"
 
 if not os.path.isdir(DIST_DIR):
     os.makedirs(DIST_DIR)
 
-with open(f"{DIST_DIR}/index.html", "w", encoding="utf-8") as f:
-    f.write(HTML)
+def generate_html_file(md_filename: str) ->  None:
+    with open(f"{PAGES_PATH}/{md_filename}", encoding="utf-8") as f:
+        md = f.read()
+    html = f"""<!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    {markdown2.markdown(md)}
+    </body>
+    </html>"""
+    html_filename = md_filename.replace(".md", ".html")
+    with open(f"{DIST_DIR}/{html_filename}", "w", encoding="utf-8") as f:
+        f.write(html)
+
+generate_html_file("index.md")
 
 print("Done generating site")
